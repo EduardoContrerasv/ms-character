401da757f88a89711f62944f28d5e7b082a9b8b2
Dario Sagredo <dariosagredo@gmail.com>
2026-05-19 20:05:04 -0400

    Conexion FeingClient creada con ms-user (UserFeignClient) UserDto agregado para la conexión con ms-user Notacion @EnableFeignClients agregada al main de characters Método para crear characters modificado, ahora verifica si existe una id de usuario en el ms-user y lo crea, si no existe retorna un error CharacterRepository ahora devuelve una lista usuarios cuando buscas por id y por nombre

--- src/main/java/cl/duoc/ms_characters/MsCharactersApplication.java
+++ src/main/java/cl/duoc/ms_characters/MsCharactersApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan(basePackages = "cl.duoc.ms_characters.model")
 @EnableJpaRepositories(basePackages = "cl.duoc.ms_characters.repository")
+@EnableFeignClients
 public class MsCharactersApplication {
 
 	public static void main(String[] args) {
